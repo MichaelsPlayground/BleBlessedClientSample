6c792f97be520289485c0655475b3d30473ecd71
michaelfehr <michael_fehrgmx.de>
2022-10-18 18:48:29 +0200

    Initial commit

---
+++ app/src/main/java/de/androidcrypto/bleblessedclientsample/BluetoothHandlerOwn.java
@@ -0,0 +1,11 @@
+package de.androidcrypto.bleblessedclientsample;
+
+import android.bluetooth.le.ScanResult;
+
+import com.welie.blessed.BluetoothCentralManagerCallback;
+import com.welie.blessed.BluetoothPeripheral;
+
+public class BluetoothHandlerOwn {
+
+
+}
---
+++ app/src/main/java/de/androidcrypto/bleblessedclientsample/MainActivity.java
@@ -0,0 +1,364 @@
+package de.androidcrypto.bleblessedclientsample;
+
+import static android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE;
+import static com.welie.blessed.BluetoothBytesParser.FORMAT_SINT16;
+import static com.welie.blessed.BluetoothBytesParser.FORMAT_UINT16;
+import static com.welie.blessed.BluetoothBytesParser.FORMAT_UINT8;
+import static com.welie.blessed.BluetoothBytesParser.bytes2String;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.welie.blessed.BluetoothBytesParser;
+import com.welie.blessed.BluetoothCentralManager;
+
+import com.welie.blessed.BluetoothCentralManagerCallback;
+import com.welie.blessed.BluetoothPeripheral;
+import com.welie.blessed.BluetoothPeripheralCallback;
+import com.welie.blessed.ConnectionPriority;
+import com.welie.blessed.GattStatus;
+import com.welie.blessed.ScanMode;
+import com.welie.blessed.WriteType;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.TimeZone;
+import java.util.UUID;
+
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button scanForDevices;
+    TextView heartRateMeasurement;
+    String heartRateMeasurementString;
+
+
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int ACCESS_LOCATION_REQUEST = 2;
+
+    /**
+     * Return Intent extra
+     */
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+    String macAddressFromScan = ""; // will get filled by Intent from DeviceScanActivity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        heartRateMeasurement = findViewById(R.id.tvMainHeartRateMeasurement);
+
+        registerReceiver(locationServiceStateReceiver, new IntentFilter((LocationManager.MODE_CHANGED_ACTION)));
+        registerReceiver(heartRateDataReceiver, new IntentFilter( BluetoothHandler.MEASUREMENT_HEARTRATE ));
+
+
+        //registerReceiver(locationServiceStateReceiver, new IntentFilter((LocationManager.MODE_CHANGED_ACTION)));
+        //registerReceiver(heartRateDataReceiver, new IntentFilter( BluetoothHandler.MEASUREMENT_HEARTRATE ));
+        /*
+        registerReceiver(locationServiceStateReceiver, new IntentFilter((LocationManager.MODE_CHANGED_ACTION)));
+        registerReceiver(bloodPressureDataReceiver, new IntentFilter( BluetoothHandler.MEASUREMENT_BLOODPRESSURE ));
+        registerReceiver(temperatureDataReceiver, new IntentFilter( BluetoothHandler.MEASUREMENT_TEMPERATURE ));
+        registerReceiver(heartRateDataReceiver, new IntentFilter( BluetoothHandler.MEASUREMENT_HEARTRATE ));
+        registerReceiver(pulseOxDataReceiver, new IntentFilter( BluetoothHandler.MEASUREMENT_PULSE_OX ));
+        registerReceiver(weightDataReceiver, new IntentFilter(BluetoothHandler.MEASUREMENT_WEIGHT));
+        registerReceiver(glucoseDataReceiver, new IntentFilter(BluetoothHandler.MEASUREMENT_GLUCOSE));
+        */
+
+
+        scanForDevices = findViewById(R.id.btnMainScan);
+        scanForDevices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, DeviceScanActivityOwn.class);
+                startActivity(intent);
+                /*
+                BluetoothHandler bth = BluetoothHandler.getInstance(view.getContext());
+                BluetoothCentralManager bcm = bth.central;
+                bcm.stopScan();
+                bcm.close();
+                registerReceiver(locationServiceStateReceiver, new IntentFilter((LocationManager.MODE_CHANGED_ACTION)));
+                registerReceiver(heartRateDataReceiver, new IntentFilter( BluetoothHandler.MEASUREMENT_HEARTRATE ));
+
+                bcm.scanForPeripherals();
+                */
+            }
+        });
+
+        // receive the address from DeviceListOwnActivity, if we receive data run the connection part
+        Intent incommingIntent = getIntent();
+        Bundle extras = incommingIntent.getExtras();
+        if (extras != null) {
+            macAddressFromScan = extras.getString(EXTRA_DEVICE_ADDRESS); // retrieve the data using keyName
+            System.out.println("Main received data: " + macAddressFromScan);
+            try {
+                if (!macAddressFromScan.equals("")) {
+                    Log.i("Main", "selected MAC: " + macAddressFromScan);
+                    initBluetoothHandler(macAddressFromScan);
+                }
+            } catch (NullPointerException e) {
+                // do nothing, there are just no data
+            }
+        }
+
+    }
+
+
+    private void printToast(String message) {
+        Toast.makeText(getApplicationContext(),
+                        message,
+                        Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    /**
+     * section for (runtime) permissions
+     */
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] missingPermissions = getMissingPermissions(getRequiredPermissions());
+            if (missingPermissions.length > 0) {
+                requestPermissions(missingPermissions, ACCESS_LOCATION_REQUEST);
+            } else {
+                permissionsGranted();
+            }
+        }
+    }
+
+    private String[] getMissingPermissions(String[] requiredPermissions) {
+        List<String> missingPermissions = new ArrayList<>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (String requiredPermission : requiredPermissions) {
+                if (getApplicationContext().checkSelfPermission(requiredPermission) != PackageManager.PERMISSION_GRANTED) {
+                    missingPermissions.add(requiredPermission);
+                }
+            }
+        }
+        return missingPermissions.toArray(new String[0]);
+    }
+
+    private String[] getRequiredPermissions() {
+        int targetSdkVersion = getApplicationInfo().targetSdkVersion;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && targetSdkVersion >= Build.VERSION_CODES.S) {
+            return new String[]{Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT};
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && targetSdkVersion >= Build.VERSION_CODES.Q) {
+            return new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+        } else return new String[]{Manifest.permission.ACCESS_COARSE_LOCATION};
+    }
+
+    private void permissionsGranted() {
+        // Check if Location services are on because they are required to make scanning work for SDK < 31
+        int targetSdkVersion = getApplicationInfo().targetSdkVersion;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S && targetSdkVersion < Build.VERSION_CODES.S) {
+            if (checkLocationServices()) {
+                //initBluetoothHandler();
+                printToast("start scan to proceed");
+            }
+        } else {
+            //initBluetoothHandler();
+            printToast("start scan to proceed");
+        }
+    }
+
+    private boolean areLocationServicesEnabled() {
+        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager == null) {
+            Timber.e("could not get location manager");
+            return false;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            return locationManager.isLocationEnabled();
+        } else {
+            boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            return isGpsEnabled || isNetworkEnabled;
+        }
+    }
+
+    private boolean checkLocationServices() {
+        if (!areLocationServicesEnabled()) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Location services are not enabled")
+                    .setMessage("Scanning for Bluetooth peripherals requires locations services to be enabled.") // Want to enable?
+                    .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // if this button is clicked, just close
+                            // the dialog box and do nothing
+                            dialog.cancel();
+                        }
+                    })
+                    .create()
+                    .show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Check if all permission were granted
+        boolean allGranted = true;
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                allGranted = false;
+                break;
+            }
+        }
+
+        if (allGranted) {
+            permissionsGranted();
+        } else {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Permission is required for scanning Bluetooth peripherals")
+                    .setMessage("Please grant permissions")
+                    .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                            checkPermissions();
+                        }
+                    })
+                    .create()
+                    .show();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (getBluetoothManager().getAdapter() != null) {
+            if (!isBluetoothEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                checkPermissions();
+            }
+        } else {
+            Timber.e("This device has no Bluetooth hardware");
+        }
+    }
+
+    private boolean isBluetoothEnabled() {
+        BluetoothAdapter bluetoothAdapter = getBluetoothManager().getAdapter();
+        if(bluetoothAdapter == null) return false;
+
+        return bluetoothAdapter.isEnabled();
+    }
+
+    private void initBluetoothHandler()
+    {
+        BluetoothHandler.getInstance(getApplicationContext());
+    }
+
+    private void initBluetoothHandler(String macAddress)
+    {
+        BluetoothHandler.getInstance(getApplicationContext(), macAddress);
+    }
+
+    @NotNull
+    private BluetoothManager getBluetoothManager() {
+        return Objects.requireNonNull((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE),"cannot get BluetoothManager");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(heartRateDataReceiver);
+        unregisterReceiver(locationServiceStateReceiver);
+        /*
+        unregisterReceiver(bloodPressureDataReceiver);
+        unregisterReceiver(temperatureDataReceiver);
+        unregisterReceiver(pulseOxDataReceiver);
+        unregisterReceiver(weightDataReceiver);
+        unregisterReceiver(glucoseDataReceiver);
+         */
+    }
+
+    /**
+     * section for peripheral
+     */
+
+    private BluetoothPeripheral getPeripheral(String peripheralAddress) {
+        BluetoothCentralManager central = BluetoothHandler.getInstance(getApplicationContext()).central;
+        return central.getPeripheral(peripheralAddress);
+    }
+
+    /**
+     * section for broadcast receiver = all included ble services need one
+     */
+
+    private final BroadcastReceiver locationServiceStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action != null && action.equals(LocationManager.MODE_CHANGED_ACTION)) {
+                boolean isEnabled = areLocationServicesEnabled();
+                Timber.i("Location service state changed to: %s", isEnabled ? "on" : "off");
+                checkPermissions();
+            }
+        }
+    };
+
+    private final BroadcastReceiver heartRateDataReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            HeartRateMeasurement measurement = (HeartRateMeasurement) intent.getSerializableExtra(BluetoothHandler.MEASUREMENT_HEARTRATE_EXTRA);
+            Log.i("Main", "heartRateDataReceiver");
+            if (measurement == null) return;
+            heartRateMeasurementString = String.format(Locale.ENGLISH, "%d bpm", measurement.pulse);
+            heartRateMeasurement.setText(heartRateMeasurementString);
+            Log.i("Main", "heartRateMeasurement: " + heartRateMeasurement);
+            //measurementValue.setText(String.format(Locale.ENGLISH, "%d bpm", measurement.pulse));
+        }
+    };
+}
